feb3943bd4433285bda78762e9dd397002efda52
Chlarence Callelero <111835302+chlarence@users.noreply.github.com>
2023-12-17 13:20:15 +0800

    Add files via upload

---
+++ Calle-BPnn/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Calle_BPnn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(74, 126);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Train";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(74, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(193, 62);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(193, 183);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(132, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(193, 144);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(132, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(193, 104);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(132, 22);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(193, 232);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(132, 22);
+            this.textBox5.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 232);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Output";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Inputs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 262);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "?";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 321);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Calle-BPnn/Form1.cs
@@ -0,0 +1,135 @@
+﻿using Backprop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calle_BPnn
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet nn;
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 400;
+            this.Height = 280;  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(4, 125, 1);
+        
+            if (nn != null)
+            {
+                button1.Enabled = false;
+
+                button2.Enabled = true;
+                button3.Enabled = true;
+
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox3.Enabled = true;
+                textBox4.Enabled = true;
+              
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double[,] weights = new double[4, 1];
+            double learningRate = 0.1;
+
+            for (int epoch = 0; epoch < 100; epoch++)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    double[] inputs = new double[4];
+                    for (int j = 0; j < 4; j++)
+                    {
+                        inputs[j] = (i & (1 << j)) > 0 ? 1.0 : 0.0;
+                    }
+
+                    double desiredOutput = i == 15 ? 1.0 : 0.0;
+
+                    double output = CalculateOutput(inputs, weights);
+
+                    // Update weights using the perceptron learning rule
+                    for (int j = 0; j < 4; j++)
+                    {
+                        weights[j, 0] += learningRate * (desiredOutput - output) * inputs[j];
+                    }
+                }
+            }
+
+            label3.Text = "Successfully trained using perceptron learning!";
+        }
+
+        // Assume you have a method like this for calculating the output
+        private double CalculateOutput(double[] inputs, double[,] weights)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                sum += inputs[i] * weights[i, 0];
+            }
+            return sum > 0.5 ? 1.0 : 0.0; // Threshold for binary output
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double input1 = Convert.ToDouble(textBox1.Text);
+                double input2 = Convert.ToDouble(textBox2.Text);
+                double input3 = Convert.ToDouble(textBox3.Text);
+                double input4 = Convert.ToDouble(textBox4.Text);
+
+                Validate(input1);
+                Validate(input2);
+                Validate(input3);
+                Validate(input4);
+
+                // Simple AND logic with sigmoid activation
+                double result = Sigmoid(input1 + input2 + input3 + input4);
+
+                textBox5.Text = result.ToString("0.000000000000");
+
+                label3.Text = "Successfully ran logic AND with sigmoid activation!";
+                timer1.Enabled = true;
+                timer1.Start();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Invalid input. Please enter valid numbers.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+        private double Sigmoid(double x)
+        {
+            return 1 / (1 + Math.Exp(-x));
+        }
+
+        private void Validate(double input)
+        {
+            if (input != 0 && input != 1)
+            {
+                throw new ArgumentException("Invalid input. Please enter either 0 or 1.");
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = " ";
+            timer1.Stop();
+        }
+    }
+}
